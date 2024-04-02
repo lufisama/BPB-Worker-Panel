759e6e91f08b95ef2aadf7ff52046ab66440fc04
lufisama <165838464+lufisama@users.noreply.github.com>
2024-04-02 12:41:28 -0700

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+let userID = "c4d602f9-d6c7-4e81-85b8-7c603e2ffe64";
 
 //https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 //https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
